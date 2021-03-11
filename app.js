91ebf1705ea6cd78ce85d8d4fc9d6b274f0d0ec9
dilanunver <dunver33@gmail.com>
2021-03-11 16:20:32 +0300

    adding colors of products

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const products = [
           'img': 'https://gearmoose.com/wp-content/uploads/2019/08/Proof-Rover-Pants.jpg',
           'stock': 5
         }},
-    price: 11.59
+    price: 11.59,
+    selectedColor: 'yellow'
   }, 
   {
     id : '2',
@@ -27,7 +28,8 @@ const products = [
         'img': 'https://rendering.documents.cimpress.io/v1/vp/preview?scene=https://scene.products.cimpress.io/v1/scenes/3dba1cd7-7694-4313-aab9-1d0754b868c7&width=690&height=690&quality=80',
         'stock': 5
       }},
-    price: 17.59
+    price: 17.59,
+    selectedColor: 'blue'
   }, 
   {
     id : '3',
@@ -42,7 +44,8 @@ const products = [
         'img': 'https://www.bluemint.com/content/images/thumbs/001/0011120_edward_1200.jpeg',
         'stock': 5
       }},
-    price: 8.19
+    price: 8.19,
+    selectedColor: 'red'
   }, 
   {
     id : '4',
@@ -57,7 +60,8 @@ const products = [
         'img': 'https://cdn.shopify.com/s/files/1/0260/8116/5402/products/product-image-1391418095_2000x.jpg?v=1595519030',
         'stock': 3
       }},
-    price: 1.59    
+    price: 1.59,
+    selectedColor: 'blue'    
   }, 
   {
     id : '5',
@@ -72,7 +76,8 @@ const products = [
         'img': 'https://cdn.kobisi.com/cdn/image/546/4559717/1/1200/1200/zigzagged-stitch-skirt-black.jpg',
         'stock': 4
       }},
-    price: 7.59    
+    price: 7.59,
+    selectedColor: 'orange'    
   }, 
   {
     id : '6',
@@ -87,16 +92,15 @@ const products = [
         'img': 'https://ktnimg.mncdn.com/mnresize/868/1140/product-images/9KAK95028AA999/1500Wx1969H/9KAK95028AA999_G01_zoom1_V02.jpg',
         'stock': 4
       }},
-    price: 3.56
+    price: 3.56,
+    selectedColor: 'red'
   }, 
 ]
-console.log(products[5].colors.red.img)
-
-
 
 let renderingArrayOfItems = [];
 const storingItems = document.querySelector('.storing-items');
 const shoppingHolder = document.querySelector('.shopping-holder');
+
 
 function renderShoppingProducts (){
   shoppingHolder.innerHTML = '';
@@ -106,27 +110,43 @@ function renderShoppingProducts (){
     element.classList.add('products')
     element.setAttribute('data-id',  item.id)
     element.innerHTML = `
-    <img src=${Object.values(item.colors)[0].img} alt=${item.name} style="width: 100px; height: 100px;">
+    <img src=${item.colors[item.selectedColor].img } alt=${item.name} style="width: 100px; height: 100px;">
      <h1>${item.name}</h1>
-     <h2><button type="button" class="dropDown">chose a color</button>
-     <div id="myDropdown" class="dropdown-content" style="display:flex; flex-direction:column;  ">
-    <a href="#" style="text-decoration:none; padding:7px 5px;">${Object.keys(item.colors)[0]}</a>
-    <a href="#" style="text-decoration:none; padding:7px 5px;">${Object.keys(item.colors)[1]}</a>
-  </div> </h2>
-     <h3>Price: ${item.price}</h3>
+     <label for="colors">Choose a color</label>
+     <select id="colors">
+     ${Object.keys(item.colors).map(color => (`
+      <option ${item.selectedColor === color ? 'selected' : ''} value=${color} >${color}</option>
+      `
+     ))}   
+     </select>
+     <h3>Price: $${item.price}</h3>
      <h4>Stock: ${item.stock }
      ${item.stock <= 0 ? 
       `<button type="button" class="buyButton" disabled >Buy</button>` : 
       `<button type="button" class="buyButton" style="cursor: pointer;">Buy</button>`}</h4> 
      <h5>Category : ${item.category}</h5>
      `
-     console.log(Object.keys(item.colors)[0])
+     const colors = element.querySelector("#colors");
+    
+     colors.addEventListener('change', chooseOneColor);
      const buyButton = element.querySelector('.buyButton');
      buyButton.addEventListener('click', buyingItems)
      shoppingHolder.appendChild(element)
   })
 }
 renderShoppingProducts();
+
+function chooseOneColor(e){
+  const element = e.target.parentElement;
+  const id = element.dataset.id;
+  let selectingItem = products.find( item => {
+    return id === item.id
+  })
+  selectingItem.selectedColor =   e.target.value;
+  console.log(selectingItem.colors[selectingItem.selectedColor].img)
+  renderShoppingProducts()
+}
+
 
 function renderSelectingItems(){
   storingItems.innerHTML= '';
@@ -136,12 +156,13 @@ function renderSelectingItems(){
     element.classList.add('products')
     element.setAttribute('data-id',  item.id)
     element.innerHTML = `
-    <img src=${Object.values(item.colors)[0].img} alt=${item.name} style="width: 100px; height: 100px;">
+    <img src=${item.colors[item.selectedColor].img} alt=${item.name} style="width: 100px; height: 100px;">
      <h1>${item.name}</h1>
-     <h2>Colors : ${Object.keys(item.colors)[0]} </h2>
-     <h3>Price: ${item.price}</h3>
+     <h2>color : ${item.selectedColor} </h2>
+     <h3>Price: $${item.price}</h3>
      <h4>Quantity: ${item.stock} </h4>
       `
+    
      storingItems.appendChild(element)
   })
   renderShoppingProducts()
